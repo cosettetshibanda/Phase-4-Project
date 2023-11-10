8e21e067df714a40e8c4e4195f1e85a1eeae5229
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-09 22:23:07 -0700

    login form

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,74 +1,29 @@
 import {useState} from "react";
+import LoginForm from "./LoginForm";
 
 
 function Login({onLogin}){
-    const [isLoading, setIsLoading] = useState 
-    const [errors, setErrors] = useState([])
-    const [formData, setFormData] = useState({
-        user_name: "",
-        password: '',
-      });
-
-      useEffect(() => {
-        if(!loading && loggedIn) {
-            navigate("/")
-        }
+    const [showLogin, setShowLogin] = useState(true)
     
-        return () => {
-            setErrors([])
-        }
-    }, [loading, loggedIn, navigate, setErrors]);
-    
-
-      const handleChange = (event) => {
-        setFormData({...formData, [event.target.name]: event.target.value})
-      };
-    
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setIsLoading(true)
-        fetch("/login", {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(formData)
-        }).then((r) => {
-            setIsLoading(false);
-            if (r.ok) {
-              r.json().then((user) => onLogin(user));
-            } else {
-              r.json().then((err) => setErrors(err.errors));
-            }
-          });
-        }
-    
-
-
     return (
         <div>
-     <form onSubmit={handleSubmit}>
-      <label>Username</label>
-      <input
-        type="text"
-        name="user_name"
-        value={formData.user_name}
-        onChange={handleChange}
-        required
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
-      <input type="submit" value="Login" />
-    </form>
+            <LoginForm onLogin={onLogin} />
+            <p className="accountQuestion">
+                Don't have and account? 
+            </p>
+            <button 
+                type="button"
+                onClick={() => setShowLogin(false)}
+            >
+                    Sign Up
+            </button>
+            
+            
+
+            
         </div>
     )
+      
 }
 
 export default Login
