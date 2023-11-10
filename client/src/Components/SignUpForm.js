f7f75802f8fe4207d3304e3c46f3cc1c0c5c52e3
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-10 00:31:31 -0700

    signup form

--- client/src/Components/SignUpForm.js
+++ client/src/Components/SignUpForm.js
@@ -1,6 +1,78 @@
-function SignUpForm() {
+import React, { useState } from "react";
+
+function SignUpForm({onLogin}) {
+    const [formData, setFormData] = useState({
+        user_name: "",
+        password: '',
+        passwordConfirmation: "",
+        imageUrl: "",
+        bio: "",
+      });
+
+
+      const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => onLogin(user))
+            }
+        })
+      }
     return (
-        <div></div>
+        <form onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input
+                type="text"
+                name="user_name"
+                value={formData.user_name}
+                onChange={handleChange}
+                required
+            />
+            <label>Password</label>
+            <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+            />
+            <label>Password Confirmation</label>
+            <input
+                type="password"
+                name="password_confirmation"
+                value={formData.password_confirmation}
+                onChange={handleChange}
+                required
+            />
+            <label>Profile Image</label>
+            <input
+                type="text"
+                name="imageUrl"
+                value={formData.imageUrl}
+                onChange={handleChange}
+                required
+            />
+            <label>Bio</label>
+            <input
+                type="test"
+                name="bio"
+                value={formData.bio}
+                onChange={handleChange}
+                required
+            />
+            <input type="submit" value="Login" />
+        </form>
     )
 }
 
