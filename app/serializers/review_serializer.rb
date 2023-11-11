658aad2cc70ff8691d25be3503f741f4e079a7c8
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-10 20:28:57 -0700

    added relationships

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :stars, :summary, :user_id, :carseat_id
+  belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :bio, :image_url
+  has_many :reviews
 end
