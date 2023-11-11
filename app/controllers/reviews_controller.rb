dc926ccd21fe2c271f9d8554ba0740abd850438f
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-10 21:06:43 -0700

    review controller routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,40 @@
 class ReviewsController < ApplicationController
+
+    def index
+        reviews = Review.all 
+        render json: reviews, status: ;ok
+    end
+
+    def show
+        review = review_find
+        render json: review, status: :ok
+    end
+
+    def update
+        review = review_find
+        review.update!(review_params)
+        render json: review, status: :ok
+    end
+
+    def create
+        review = Review.create!(review_params)
+        render json: review, status: :created
+    end
+
+    def destroy
+        review = review_find
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def review_find
+        Review.find(params[:id])
+    end
+
+    def review_params
+        params.permit(:summary, :stars, :user_id, carseat_id)
+    end
+        
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :user
+
+    validates :stars, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
