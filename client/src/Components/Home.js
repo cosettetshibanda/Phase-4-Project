842238c70cab478818bea99e84bbc6c9f04dde1e
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-17 02:52:44 -0700

    fixed welcome message

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   const {loggedIn} = useContext(UsersContext);
 
   return (
-    <div className='home'>{ loggedIn ? <h3>Welcome. Discover car seats you might be interested in buying. And review car seats you have already bought.</h3> : <h3>Login or Signup To Get Started!</h3>}</div>
+    <div className='home'>{ loggedIn ? <h3>Welcome! Discover car seats that will keep your children safe and work best in your family. Please leave reviews on car seats you have already bought so that others can learn from your experience.</h3> : <h3>Login or Signup To Get Started!</h3>}</div>
   )
 }
 
